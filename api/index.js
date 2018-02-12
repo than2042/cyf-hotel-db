6a87a1422684c727e72ed66240d6804e88d87c4a
Pedro <ams.pedro@gmail.com>
2018-02-12 20:37:59 +0000

    added class 3 to index

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const class2 = require("./class2");
+const class3 = require("./class3");
 router.use("/", class2);
 
 module.exports = router;
