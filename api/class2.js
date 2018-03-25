f975914e8dfa5440869cdd41e4b2bd62210c53f0
Pedro <ams.pedro@gmail.com>
2018-03-25 16:54:01 +0100

    Added imports to code base instead of being on syllabus snippet; and added database file in directory as well.

--- api/class2.js
+++ api/class2.js
@@ -1,7 +1,10 @@
 const express = require('express');
-
 const router = express.Router();
 
+const filename = 'database/database.sqlite';
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database(filename);
+db.run("PRAGMA foreign_keys = ON");
 
 router.get('/customers', function(req, res) {
   // TODO: fix code here
