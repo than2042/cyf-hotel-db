9d6f152634a9a2e305d1789a553911735fafad0d
Sue <than.than2042@gmail.com>
2018-08-15 15:25:59 +0100

    WIP

--- api/class2.js
+++ api/class2.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const filename = 'database/database.sqlite';
 const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database(filename);
-// db.run("PRAGMA foreign_keys = ON");
+db.run("PRAGMA foreign_keys = ON");
 
 router.get('/customers', function(req, res) {
   var sql = 'select * from customers';
@@ -24,20 +24,40 @@ router.get('/customers', function(req, res) {
 
 router.get('/customers/:id', function(req, res) {
   // TODO: add code here
+  var sql = `select * from customers where id = ${req.params.id}`;
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+        console.log('ERROR fetching from the database:', err);
+        return;
+    }
+    console.log('Request succeeded, new data fetched', rows);
+    res.status(200).json(
+       rows[0]
+    );
+  });
 });
 
-
-router.get('/customers/:id', function(req, res) {
+router.get('/customers/search/:surname', function(req, res) {
+  console.log(req.params.surname);
   // TODO: add code here
+  var sql = `select * from customers where surname like '%${req.params.surname}%'`;
+
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+        console.log('ERROR fetching from the database:', err);
+        return;
+    }
+    console.log('Request succeeded, new data fetched', rows);
+    res.status(200).json(
+       {customers: rows}
+    );
+  });
 });
-
-
-router.get('/customers/:surname', function(req, res) {
-  // TODO: add code here
-});
-
 
 router.post('/customers/', function(req, res) {
+  var sql = `INSERT INTO customers
+  (title, first_name, surname, email) VALUES ('${req.body.title}', '${req.body.firstName}', '${req.body.surname}', '${req.body.email}')`;
+  db.run(sql);
   // EXPECTED JSON Object:
   // {
   //   title: 'Mr',
@@ -45,12 +65,18 @@ router.post('/customers/', function(req, res) {
   //   surname: 'Ainley',
   //   email: 'laurie@ainley.com'
   // }
-
   // TODO: add code here
+  console.log(sql);
+  console.log(req.body);
+  res.status(200).json(
+    {invoices: 2}
+  );
 });
 
-
+// update data 
 router.put('/customers/:id', function(req, res) {
+  var sql = `update customers set surname = '${req.body.surname}' where id = ${req.body.id}`;
+  db.run(sql);
   // EXPECTED JSON Object:
   // {
   //   title: 'Mr',
@@ -60,30 +86,96 @@ router.put('/customers/:id', function(req, res) {
   // }
 
   // TODO: add code here
+  console.log(req.body);
+  res.status(200).json(
+    {rooms: 2}
+  );
 });
 
-
-// get '/reservations'
-// TODO: add code here
-
+// get '/reservation
+router.get('/reservations/', function(req, res) {
+  // TODO: add code here
+  var sql = `select * from reservations`;
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+        console.log('ERROR fetching from the database:', err);
+        return;
+    }
+    console.log('Request succeeded, new data fetched', rows);
+    res.status(200).json(
+       rows
+    );
+  });
+});
 
 // get '/reservations/:id'
-// TODO: add code here
-
+router.get('/reservations/:id', function(req, res) {
+  // TODO: add code here
+  var sql = `select * from reservations where id = ${req.params.id}`;
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+        console.log('ERROR fetching from the database:', err);
+        return;
+    }
+    console.log('Request succeeded, new data fetched', rows);
+    res.status(200).json(
+       rows[0]
+    )
+  });
+});
 
 // delete '/reservations/:id'
 // TODO: add code here
-
+router.delete('/reservations/:id', function(req, res) {
+  var sql = `DELETE from reservations where id = ${req.params.id}`;
+  console.log(sql);
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+        console.log('ERROR fetching from the database:', err);
+        return;
+    }
+    console.log('Request succeeded, new data fetched', rows);
+    res.status(200).json(
+       {customers: rows}
+    )
+  });
+});
 
 // get '/reservations/starting-on/:startDate'
 // TODO: add code here
-
+router.get("/reservations/starting-on/:check_in_date", function(req, res) {
+  var sql = `select form reservations where check_in_date = '${req.params.check_in_date}'`;
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+        console.log('ERROR fetching from the database:', err);
+        return;
+    }
+    console.log('Request succeeded, new data fetched', rows);
+    res.status(200).json(
+       {customers: rows}
+    )
+  });
+});
 
 // get '/reservations/active-on/:date'
 // TODO: add code here
-
+router.get("/reservations/active-on/:date", function(req, res) {
+  // TODO: add code here
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+        console.log('ERROR fetching from the database:', err);
+        return;
+    }
+    console.log('Request succeeded, new data fetched', rows);
+    res.status(200).json(
+       {customers: rows}
+    )
+  });
+});
 
 // post '/reservations'
+router.post('/reservations/', function(req, res) {
+// TODO: add code here
 // EXPECTED JSON Object:
 // {
 //   customer_id: 1,
@@ -92,7 +184,20 @@ router.put('/customers/:id', function(req, res) {
 //   check_out_date: '2018-01-22',
 //   room_price: 129.90
 // }
-// TODO: add code here
+  var sql = `INSERT INTO reservations
+  (customer_id, room_id, check_in_date, check_out_date, room_price) VALUES ('${req.body.customer_id}', '${req.body.room_id}', '${req.body.check_in_date}', '${req.body.check_out_date}', '${req.body.room_price}')`;
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+        console.log('ERROR fetching from the database:', err);
+        return;
+    }
+    console.log('Request succeeded, new data fetched', rows);
+    res.status(200).json(
+       {customers: rows}
+    )
+  });
+});
+
 
 
 // get `/detailed-invoices'
@@ -103,3 +208,5 @@ router.put('/customers/:id', function(req, res) {
 // TODO: add code here
 
 module.exports = router;
+
+//select * from customers where surname
--- database/schema.sql
+++ database/schema.sql
@@ -1,17 +1,112 @@
-CREATE TABLE IF NOT EXISTS customers (
-  ID INTEGER PRIMARY KEY AUTOINCREMENT,
-  title TEXT(16),
-  first_name TEXT(64) NOT NULL,
-  surname TEXT (64) NOT NULL,
-  email TEXT (64)
+PRAGMA foreign_keys = ON;
+DROP table IF EXISTS customers;
+CREATE TABLE
+IF NOT EXISTS customers(
+  id INTEGER PRIMARY KEY AUTOINCREMENT , 
+  title TEXT(16), 
+  first_name TEXT(64) NOT NULL, 
+  surname TEXT(64) NOT NULL, 
+  email TEXT(64)
 );
 
+INSERT INTO customers(title, first_name, surname, email) VALUES ('Mr', 'John', 'Dove', 'john.doe@domain.com');
+INSERT INTO customers(title, first_name, surname, email) VALUES ('Mr', 'Jim', 'Frlton', 'jim.f@domain.com');
+INSERT INTO customers(title, first_name, surname, email) VALUES ('Mr', 'Keke', 'Lili', 'keke.lili@domain.com');
+INSERT INTO customers (title, first_name, surname, email) VALUES ('Ms',    'Joanna','Smith','j.smith@domain.com');
+INSERT INTO customers (title, first_name, surname, email) VALUES ('Ms',    'Anna','Kolen','a.col@domain.com');
+INSERT INTO customers (title, first_name, surname, email) VALUES ('Ms',    'Marie','Niki','m.niki@domain.com');
+INSERT INTO customers (title, first_name, surname, email) VALUES ('Ms',    'Cherie','Mart','c.mart@domain.com');
 
-INSERT INTO customers (
-  title, first_name, surname, email
-) VALUES (
-  'Mr',
-  'John',
-  'Dove',
-  'john.doe@domain.com'
+-- select * from customers;
+
+DROP table IF EXISTS room_types;
+CREATE TABLE IF NOT EXISTS room_types(
+  id INTEGER PRIMARY KEY AUTOINCREMENT ,
+  type_name TEXT(64) NOT NULL,
+  original_price number,
+  current_price number
 );
+INSERT INTO room_types (type_name, original_price, current_price) VALUES ('Single room', 40, 50.50 );
+INSERT INTO room_types (type_name, original_price, current_price) VALUES ('Double room', 60, 80 );
+INSERT INTO room_types (type_name, original_price, current_price) VALUES ('Suite', 100, 120 );
+
+-- select * from room_types;
+DROP table IF EXISTS rooms;
+CREATE TABLE IF NOT EXISTS rooms(
+  id INTEGER PRIMARY KEY AUTOINCREMENT ,
+  room_type_id INTEGER NOT NULL,  
+  seaview boolean,
+  foreign key(room_type_id) references room_types(id)
+);
+
+INSERT INTO rooms (room_type_id, seaview) VALUES (1, 0);
+INSERT INTO rooms (room_type_id, seaview) VALUES (2, 0);
+INSERT INTO rooms (room_type_id, seaview) VALUES (2, 1);
+INSERT INTO rooms (room_type_id, seaview) VALUES (1, 1);
+INSERT INTO rooms (room_type_id, seaview) VALUES (2, 0);
+INSERT INTO rooms (room_type_id, seaview) VALUES (2, 1);
+INSERT INTO rooms (room_type_id, seaview) VALUES (1, 1);
+
+
+-- select * from rooms;
+DROP table IF EXISTS reservations;
+
+create table IF NOT EXISTS reservations (
+      id integer primary key AUTOINCREMENT ,
+      customer_id integer not null,
+      room_id integer not null,
+      check_in_date   datetime not null,
+      check_out_date   datetime,
+      room_price  number,
+      foreign key(customer_id)     references customers(id),
+      foreign key(room_id)     references rooms(id));
+
+  INSERT INTO reservations (customer_id, room_id, check_in_date, room_price) VALUES (1,2,'2018-07-10', 100);
+  INSERT INTO reservations (customer_id, room_id, check_in_date, room_price) VALUES (2,3,'2018-10-10', 200);
+  INSERT INTO reservations (customer_id, room_id, check_in_date, room_price) VALUES (3,4,'2018-07-10', 300);
+  INSERT INTO reservations (customer_id, room_id, check_in_date, room_price) VALUES (4,1,'2018-08-10', 100);
+  INSERT INTO reservations (customer_id, room_id, check_in_date, room_price) VALUES (1,2,'2018-07-10', 200);
+  INSERT INTO reservations (customer_id, room_id, check_in_date, room_price) VALUES (2,3,'2018-07-10', 500);
+  INSERT INTO reservations (customer_id, room_id, check_in_date, room_price) VALUES (3,4,'2018-07-10', 200);
+  INSERT INTO reservations (customer_id, room_id, check_in_date, room_price) VALUES (4,1,'2018-07-10', 700);
+  INSERT INTO reservations (customer_id, room_id,check_in_date, room_price) VALUES (4,1,'2018-08-10', 700);
+  INSERT INTO reservations (customer_id, room_id,check_in_date, room_price) VALUES (5,3,'2018-08-10', 700);
+  INSERT INTO reservations (customer_id, room_id,check_in_date, room_price) VALUES (15,10,'2018-08-10', 700);
+ 
+
+-- select * from reservations;
+DROP table IF EXISTS invoices;
+create table IF NOT EXISTS invoices (
+      id integer primary key AUTOINCREMENT ,
+      reservation_id integer not null,
+      total number,
+      surcharges   number,
+      invoice_date_time   datetime,
+      paid  boolean,
+      foreign key(reservation_id)     references reservations(id));
+
+INSERT INTO invoices (reservation_id, total, surcharges, invoice_date_time, paid) VALUES (1, 700, 50, '2018/05/01',1);
+INSERT INTO invoices (reservation_id, total, surcharges, invoice_date_time, paid) VALUES (2, 1000, 550, '2018/06/08',0);
+INSERT INTO invoices (reservation_id, total, surcharges, invoice_date_time, paid) VALUES (3, 200, 50, '2018/03/25',1);
+INSERT INTO invoices (reservation_id, total, surcharges, invoice_date_time, paid) VALUES (4, 7700, 50, '2018/08/07',1);
+INSERT INTO invoices (reservation_id, total, surcharges, invoice_date_time, paid) VALUES (5, 600, 50, '2018/04/14',0);
+
+
+-- select * from invoices;
+
+DROP table IF EXISTS reviews;
+create table IF NOT EXISTS reviews (
+      id integer primary key AUTOINCREMENT ,
+      customer_id integer not null,
+      room_type_id integer not null,
+      rating   integer,
+      comment text,
+      review_date   datetime,
+      foreign key(customer_id)     references customers(id),
+      foreign key(room_type_id)     references room_types(id));
+
+  INSERT into reviews (customer_id, room_type_id, rating, comment, review_date) VALUES (1, 2, 4, "Great room and beautiful hotel", '2018/05/01');
+  INSERT into reviews (customer_id, room_type_id, rating, comment, review_date) VALUES (2, 3, 3, "Beautiful hotel", '2018/05/01');
+  INSERT into reviews (customer_id, room_type_id, rating, comment, review_date) VALUES (4, 1, 5, "Wonderful service", '2018/05/01');
+  INSERT into reviews (customer_id, room_type_id, rating, comment, review_date) VALUES (3, 1, 4, "Great hotel", '2018/05/01');
+  select * from reviews;
