621d3b0d04a3bd27f24f5f90eb95015253a06336
Sue <than.than2042@gmail.com>
2018-08-30 11:39:16 +0100

    homework

--- api/class3.js
+++ api/class3.js
@@ -6,6 +6,25 @@ let db = new sqlite3.Database(filename);
 
 const router = express.Router();
 
+// delete 
+router.delete('/reservations/:id', (req, res) => {
+  const id = req.params.id;
+  const sql = `delete from reservations where id = ${id}`;
+
+  db.run(sql, (err, rows) => {
+    if (err) {
+      console.log('ERROR fetching from the database:', err);
+      return;
+    }
+
+    console.log(`Successfully removed reservation: ${id}`);
+    res.status(200).json({
+      message: "Successfully removed reservation " + id
+    });
+  });
+});
+
+// question 1
 // get '/reservations-and-invoices/'
 router.get('/reservations-and-invoices/', (req,res) => {
 var sql = `SELECT * 
@@ -26,6 +45,37 @@ db.all(sql, [], (err, rows) => {
 });
 });
 
+// question 2
+router.get('/reservations-and-invoices/total/:from_day/:to_day', (req, res) => {
+  console.log(req.params);
+  var fromDate = req.params.from_day;
+  var toDate = req.params.to_day;
+  console.log(fromDate, toDate);
+  var sql = 
+   `select SUM(total) 
+    from invoices 
+    where invoice_date_time >= '${fromDate}'
+    and invoice_date_time <= '${toDate}' 
+    and paid = 1`;
+      
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+        console.log('ERROR fetching from the database:', err);
+        return;
+    }
+    console.log('Request succeeded, new data fetched', rows);
+    res.status(200).json({
+      customers: rows
+    });
+  });
+  });
+
+  // router.get('/hello/:name', (req, res) => {
+  //   console.log(req.params);
+  //   res.send("hello " + req.params.name);
+  // })
+
+// question 3
 // get `/reservations-per-customer/`
 // TODO: add code here
 router.get('/reservations-per-customer/', (req, res) => {
@@ -53,28 +103,64 @@ router.get('/reservations-per-customer/', (req, res) => {
   });
   });
 
-
+// question 4-5
 // TODO: add code here
+router.get('/reservations/rooms-details/', (req, res) => {
+  var sql = `SELECT room_id, reservations.check_in_date, 
+  reservations.check_out_date, 
+  reservations.room_price, 
+  room_types.type_name, 
+  COUNT(*) 
+  FROM reservations 
+  JOIN rooms on rooms.id = reservations.room_id 
+  JOIN room_types on room_types.id = rooms.room_type_id
+  GROUP BY room_id`;
 
-router.delete('/reservations/:id', (req, res) => {
-  const id = req.params.id;
-  const sql = `delete from reservations where id = ${id}`;
-
-  db.run(sql, (err, rows) => {
+  db.all(sql, [], (err, rows) => {
     if (err) {
-      console.log('ERROR fetching from the database:', err);
-      return;
+        console.log('ERROR fetching from the database:', err);
+        return;
     }
-
-    console.log(`Successfully removed reservation: ${id}`);
+    console.log('Request succeeded, new data fetched', rows);
     res.status(200).json({
-      message: "Successfully removed reservation " + id
+      customers: rows
     });
   });
 });
 
+// question 6
+router.get('/rooms/', (req, res) => {
+  var sql = `SELECT sea_view, 
+  reservations.check_in_date,
+  reservations.check_out_date,
+  reservations.room_id, 
+  room_types.type_name, 
+  COUNT(*)
+  FROM reservations
+  JOIN rooms on 
+  reservations.room_id = rooms.id 
+  JOIN room_types on room_types.id = rooms.room_type_id
+  GROUP BY room_id 
+  having rooms.sea_view = 1 
+  and count (type_name) >= 5`;
+
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+        console.log('ERROR fetching from the database:', err);
+        return;
+    }
+    console.log('Request succeeded, new data fetched', rows);
+    res.status(200).json({
+      customers: rows
+    });
+  });
+});
+
+
+// TODO: add code here
+
 router.get('/reservations/:customer/', (req, res) => {
-  console.log(req)
+  console.log(req);
   const customer = req.params.customer;
   const sql = `select * from reservations where customer_id = ${customer}`;
 
@@ -91,6 +177,7 @@ router.get('/reservations/:customer/', (req, res) => {
   });
 });
 
+// question 8
 // HOMEWORK
 // get '/reservations/details-between/:from_day/:to_day'
 // TODO: add for code here
@@ -121,13 +208,19 @@ router.get('/reservations/details-between/:from_day/:to_day', (req, res) => {
   });
 });
 
-
-
+// question 9
 // HOMEWORK
 // get '/reservations-per-customer/'
 // TODO: add code here
 router.get('/reservations-per-customer/', (req, res) => {
-var sql = ``
+var sql = `SELECT customers.id,
+customers.first_name, 
+customers.surname,
+COUNT(*)
+FROM reservations
+JOIN customers on
+reservations.customer_id = customers.id 
+GROUP BY customers.id`;
 
   db.run(sql, (err, rows) => {
     if (err) {
@@ -142,11 +235,16 @@ var sql = ``
   }); 
 });
 
+// question 10
 // HOMEWORK
 // get `/stats-price-room/`
 // TODO: add code here
 router.get('/stats-price-room/', (req, res) => {
-  var sql = ``;
+  var sql = `SELECT reservations.room_id, 
+  SUM(room_price), 
+  AVG(room_price),
+  COUNT(*) 
+  FROM reservations GROUP BY room_id`;
   
   db.run(sql, (err, rows) => {
     if (err) {
@@ -159,14 +257,25 @@ router.get('/stats-price-room/', (req, res) => {
       message: "Successfully removed reservation " + id
     });
   }); 
+});
 
-})
-
+// question 11
 // HOMEWORK
 // get `/rooms/available-in/:from_day/:to_day`
 // TODO: add code here
 router.get('/rooms/available-in/:from_day/:to_day', (req, res) => {
-  var sql = ``
+  var fromDate = req.params.from_day;
+  var toDate = req.params.to_day;
+  var sql = `SELECT DISTINCT rooms.id
+  FROM rooms 
+  where rooms.id not in (
+      SELECT reservations.room_id
+      FROM reservations
+      where 
+          reservations.check_out_date between '${fromDate}' and '${toDate}'
+      and 
+          reservations.check_in_date between '${fromDate}' and '${toDate}'
+  )`;
 
   db.run(sql, (err, rows) => {
     if (err) {
